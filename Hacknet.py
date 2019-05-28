c58aedcf66e29108b8a9a26f8518ae7ec1723d2a
Bon Jonson <boniekee@gmail.com>
2019-05-29 00:05:27 +0300

    v0.1 - Списки сравниваются поэлементно - Списки сравниваются только после всех введенных кодов

--- Hacknet.py
+++ Hacknet.py
@@ -1,10 +1,25 @@
 import wget
 
-code_list = list() #Лист для хранения списка всех кодов с сервера
-code = str #переменная хранящая код
-code_inputed = list() #Лист для хранения введённых кодов
+open_file = '' #переменная для открытия файла
+code_list = '' #переменная для чтения содержимого файла
+code = '' #переменная хранящая код
+code_inputed = '' #переменная для хранения введённых кодов
 
 #wget.download = ('http://hacknet.commando21.ru", 'hacknet.txt') #Загрузка файла с кодами
-
-
-
+try:
+    open_file = open('hacknet.txt', 'r') # открываем файл и сохраняем его в переменную
+except IOError:
+    print('No file')
+#code_list.split #сохраняем строку в лист, в качестве разделителя используется пробел
+code_list = open_file.read() #считываем содержимое файла с кодами
+code_list = code_list.split() #разделяем строку на элементы и записываем в список
+print(code_list) # дебаг
+for code in code_list:
+    code = input() #вводим код
+    code_inputed = code_inputed + ' ' + code #вносим код в строку code_inputed
+code_inputed = code_inputed.split() #сохраняем строку в лист, в качестве разделителя используется пробел
+print(code_inputed) #дебаг
+if code_list == code_inputed: #сравниваем два списка
+    print("Access Granted")
+else:
+    print("You're Wrong")
