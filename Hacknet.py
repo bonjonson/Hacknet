bf91107c0385fb23a580865998ad72a642532fc6
Bon Jonson <boniekee@gmail.com>
2019-05-29 00:04:22 +0300

    v.1 - Проверка осуществляется только после всех введенных кодов; - Списки сравниваются поэлементно

---
+++ Hacknet.py
@@ -0,0 +1,10 @@
+import wget
+
+code_list = list() #Лист для хранения списка всех кодов с сервера
+code = str #переменная хранящая код
+code_inputed = list() #Лист для хранения введённых кодов
+
+#wget.download = ('http://hacknet.commando21.ru", 'hacknet.txt') #Загрузка файла с кодами
+
+
+
