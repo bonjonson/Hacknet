f70920cca4d237d49396538820148bc17f8ec4e7
Bon Jonson <boniekee@gmail.com>
2019-05-29 21:31:31 +0300

    v 0.2.2
    - Добавлена проверка на содержимое файла
    - Загрузка удаленного файла временно удалена
    - Добавлена библиотека os

--- Hacknet.py
+++ Hacknet.py
@@ -1,26 +1,27 @@
-import requests
 import sys
+import os
 
 code_list = '' #переменная для чтения содержимого файла
 code = '' #переменная хранящая код
 code_inputed = '' #переменная для хранения введённых кодов
 
 try:
-    open_file = open(r'hacknet.txt', 'wb') #открываем файл для записи
-    ufr = requests.get('http://commando21.ru/hacknet.txt') #создаем запрос
-    open_file.write(ufr.content) #записываем содержимое в файл
+    open_file = open('hacknet.txt', 'r').read() # открываем файл и сохраняем его в переменную
 except IOError:
     print('No file')
     sys.exit(0) #программа завершается, если не обнаружен файл
-#code_list.split #сохраняем строку в лист, в качестве разделителя используется пробел
-code_list = open_file #считываем содержимое файла с кодами
-code_list = code_list.split() #разделяем строку на элементы и записываем в список
-for code in code_list:
-    print('Enter your code: ') 
-    code = input() #вводим код
-    code_inputed = code_inputed + ' ' + code #вносим код в строку code_inputed
-code_inputed = code_inputed.split() #сохраняем строку в лист, в качестве разделителя используется пробел
-if code_list == code_inputed: #сравниваем два списка
-    print("Access Granted", u'\u2705')
+if os.stat('hacknet.txt').st_size > 0:
+    code_list = open_file #считываем содержимое файла с кодами
+    code_list = code_list.split() #разделяем строку на элементы и записываем в список
+    for code in code_list:
+        print('Enter your code: ') 
+        code = input() #вводим код
+        code_inputed = code_inputed + ' ' + code #вносим код в строку code_inputed
+    code_inputed = code_inputed.split() #сохраняем строку в лист, в качестве разделителя используется пробел
+    if code_list == code_inputed: #сравниваем два списка
+        print("Access Granted", u'\u2705')
+    else:
+        print("Access Denied", u'\u274c')
 else:
-    print("Access Denied", u'\u274c')
+    print('File is empty')
+    sys.exit(0) #завершаем программу, если файл пустой
